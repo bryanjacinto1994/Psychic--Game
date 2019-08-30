351aa2f1955bb9c4cdb115c9857e54b4b44b5828
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-08-30 10:03:22 -0700

    made changed to javascript link to html because it wasnt working, but now it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,21 +4,24 @@
 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "w", "z"];
 
+//Make a variable for each ID's needed on html.
+var youPressed = document.getElementById("you-pressed");
+var computerPressed = document.getElementById("computer-pressed");
+var youWin = document.getElementById("you-win");
+var youLose = document.getElementById("you-lose");
+var guessesLeft = document.getElementById("guesses-left");
+var yourGuesses = document.getElementById("your-guesses");
+
+//Make a variable for scores
+var wins = 0;
+var lose = 0;
+var attempts = 9;
+
+
+
 //Make an function the key is run when the use press a key.
 document.onkeyup = function (event) {
 
-    //Make a variable for each ID's needed on html.
-    var youPressed = document.getElementById("you-pressed");
-    var computerPressed = document.getElementById("computer-pressed");
-    var youWin = document.getElementById("you-win");
-    var youLose = document.getElementById("you-lose");
-    var guessesLeft = document.getElementById("guesses-left");
-    var yourGuesses = document.getElementById("your-guesses");
-
-    //Make a variable for scores
-    var wins = 0;
-    var lose = 0;
-    var attempts = 9;
 
     //Make a variable computerGuess and a value that randomizes letters.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -27,25 +30,28 @@ document.onkeyup = function (event) {
     var userGuess = event.key;
 
     //Make if statements for user that shows which key is pressed
-    if (userGuess === computerChoices.length) {
-        youPressed.textContent = "You Chose: " + userGuess;
-        computerPressed.textContent = "Your Opponent Chose: " + computerGuess;
-        yourGuesses.textContent = "Your Guesses So Far: " + userGuess;
-        Youwin.textConent = "Your Score: " + wins;
-        youLose.textcontent = "Losses: " + lose;
+    if ((userGuess === computerChoices.length && computerGuess === computerChoices.length)) {
 
-        if (youPressed.textcontent === computerGuess) {
+
+        if (userGuess === computerGuess) {
             wins++;
-        
-    else {
-                lose++;
-                attempts--;
-            }
-
-
-
-
         }
 
+        else if (userGuess !== computerGuess) {
+            lose++;
+        }
+       
+
+
+
     }
+    //Text-Contents
+    youPressed.textContent = "You Chose: " + userGuess;
+    computerPressed.textContent = "Your Opponent Chose: " + computerGuess;
+    yourGuesses.textContent = "Your Guesses So Far: " + userGuess;
+    youWin.textConent = "Your Score: " + wins;
+    youLose.textContent = "Losses: " + lose;
+    guessesLeft.textContent = "Your Attempts: " + attempts;
+
+
 };
